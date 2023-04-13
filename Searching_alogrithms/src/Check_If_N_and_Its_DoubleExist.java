20de6e54230f3cc45cb6f85e51c7d91641cb3b9b
ankity26 <ankity2608@gmail.com>
2023-04-13 21:59:42 +0530

    added Searching-algo directory

---
+++ Searching_alogrithms/src/Check_If_N_and_Its_DoubleExist.java
@@ -0,0 +1,44 @@
+
+import java.util.Arrays;
+public class Check_If_N_and_Its_DoubleExist
+{
+    public static void main(String[] args)
+    {
+        int[] nums = {10,2,5,3};
+        System.out.println(checkIfExist(nums));
+
+
+    }
+    static boolean checkIfExist(int[] arr)
+    {
+
+        Arrays.sort(arr); //we first sort the array in ascending order
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            int target = 2*arr[i];
+            int start = 0;
+            int end = arr.length-1;
+
+            while(start <= end)
+            {
+                int mid = start + (end - start ) / 2;
+                if(arr[mid] == target && mid != i) return true;
+
+                else if (arr[mid] < target)
+                    start ++;
+
+                else
+                    end--;
+            }
+        }
+        return false;
+    }
+    /* we use a for loop and inside that a while loop.
+       we take the first element and we multiply it by 2 and we make it our target
+       and then we'll look for our target using the while loop.
+        if we find the target and our above i and mid is not equal we return true.
+     */
+}
+
+}
---
+++ Searching_alogrithms/src/first_bad_version.java
@@ -0,0 +1,7 @@
+public class first_bad_version
+{
+    public static void main(String[] args) {
+        //https://leetcode.com/problems/first-bad-version/
+
+    }
+}
